e7c2174c605d28606374f819ba241dbe7f9dd479
Kazuki Tobita <kazukiti201@gmail.com>
2019-02-25 12:02:53 +0900

    Add 05. n-gram

---
+++ 05.py
@@ -0,0 +1,3 @@
+print((lambda str, n: [str[i:i+n] for i in range(len(str) - n + 1)])('I am an NLPer', 2))
+print((lambda str, n: [str[i:i+n] for i in range(len(str) - n + 1)])('I am an NLPer'.split(), 2))
+
