9e1493bae5d0ae6679245125be3846738497cb71
Kazuki Tobita <kazukiti201@gmail.com>
2019-02-26 21:21:12 +0900

    Modify 14

--- 14.py
+++ 14.py
@@ -1,1 +1,3 @@
+# 14. 先頭からN行を出力
+# 自然数Nをコマンドライン引数などの手段で受け取り，入力のうち先頭のN行だけを表示せよ．確認にはheadコマンドを用いよ．
 print(''.join([l for i, l in zip(range(int(__import__('sys').argv[1])), open('hightemp.txt').readlines())]))
