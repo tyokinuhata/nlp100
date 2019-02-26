69bf988c7d0703cea8b0e84b55b75cd33e3906ab
Kazuki Tobita <kazukiti201@gmail.com>
2019-02-26 20:56:19 +0900

    Add 12. 1列目をcol1.txtに，2列目をcol2.txtに保存

---
+++ 12.py
@@ -0,0 +1,3 @@
+# 12. 1列目をcol1.txtに，2列目をcol2.txtに保存
+# 各行の1列目だけを抜き出したものをcol1.txtに，2列目だけを抜き出したものをcol2.txtとしてファイルに保存せよ．確認にはcutコマンドを用いよ．
+[open('col1.txt', 'a').write(line[0]) & open('col2.txt', 'a').write(line[1]) for line in open('hightemp.txt').readlines()]
