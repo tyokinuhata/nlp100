674444707e688c89d332964d170d1c7b38f8fef2
Kazuki Tobita <kazukiti201@gmail.com>
2019-02-28 19:34:01 +0900

    Add 18. 各行を3コラム目の数値の降順にソート

---
+++ 18.py
@@ -0,0 +1,3 @@
+# 18. 各行を3コラム目の数値の降順にソート
+# 各行を3コラム目の数値の逆順で整列せよ（注意: 各行の内容は変更せずに並び替えよ）．確認にはsortコマンドを用いよ（この問題はコマンドで実行した時の結果と合わなくてもよい）．
+print(sorted([l for l in open('hightemp.txt').readlines()], reverse = True, key = lambda val: float(val.split('\t')[2])))
